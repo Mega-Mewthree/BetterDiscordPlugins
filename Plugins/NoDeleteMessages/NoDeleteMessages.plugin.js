b7ef75c3380892d5b8e1808dbf918929ad508dc7
Mega-Mewthree <Mega-Mewthree@users.noreply.github.com>
2018-07-23 09:39:00 -0700

    Fixed auto-updater

--- Plugins/NoDeleteMessages/NoDeleteMessages.plugin.js
+++ Plugins/NoDeleteMessages/NoDeleteMessages.plugin.js
@@ -30,7 +30,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
-// Updated July 20th, 2018.
+// Updated July 23rd, 2018.
 
 class NoDeleteMessages {
   getName() {
@@ -43,7 +43,7 @@ class NoDeleteMessages {
     return 'Prevents the client from removing deleted messages (until restart).\nUse ".message.NoDeleteMessages-deleted-message .markup" to edit the CSS of deleted messages.\n\nMy Discord server: https://join-nebula.surge.sh\nDM me @Lucario 🌌 V5.0.0#7902 or create an issue at https://github.com/Mega-Mewthree/BetterDiscordPlugins for support.';
   }
   getVersion() {
-    return "0.0.4";
+    return "0.0.5";
   }
   getAuthor() {
     return "Mega_Mewthree"; //Current Discord account: @Lucario 🌌 V5.0.0#7902 (438469378418409483) Wonder how long this one will last...
@@ -68,7 +68,7 @@ class NoDeleteMessages {
   }
   initialize() {
     window.updateDeletedMessages = () => this.updateDeletedMessages;
-    PluginUtilities.checkForUpdate(this.getName(), this.getVersion());
+    PluginUtilities.checkForUpdate(this.getName(), this.getVersion(), `https://raw.githubusercontent.com/Mega-Mewthree/BetterDiscordPlugins/master/Plugins/${this.getName()}/${this.getName()}.plugin.js`);
     BdApi.injectCSS("NoDeleteMessages-CSS", ".message.NoDeleteMessages-deleted-message .markup {color: #F00!important;}");
     Patcher.instead(this.getName(), InternalUtilities.WebpackModules.find(m => m.dispatch), "dispatch", (thisObject, args, originalFunction) => {
       let shouldFilter = this.filter(args[0]);
@@ -133,13 +133,13 @@ class NoDeleteMessages {
 /*
 -----BEGIN PGP SIGNATURE-----
 
-iQEzBAEBCAAdFiEEGTGecftnrhRz9oomf4qgY6FcSQsFAltRkKMACgkQf4qgY6Fc
-SQtx9QgAkfqNnpJQ2zuLXdVQfT1Hne4tDS4Z0NGPm4l9hAw1ZAZ1RMLmly2IhzTI
-uJvH2w3mqlo9TUPdjtrxYw7f6CKksbsgkQD5bup5DPgeApTY8Kba9iCS0XIPwyhR
-hMkFu3a5vInmRmirJnitbcDAFswci6PcvC3KX2Dn5eHHlZ0t7NpNlsvSDLyXwcFW
-BVlEC36cPStKNTjgVunHmXxsXS0OZpQEUy+PnaMIHl5JNA9lxBjJxfCrOeQBvLqg
-khTHC1lyljLlB/jIJ0RGydwpBGMo1eGcJKTaZ+akibxc6x7fZ5U3hQY0Wi2rW2mB
-ytCMusQZd6/z5jcYfQMdYhgVz0AX3w==
-=hCvI
+iQEzBAEBCAAdFiEEGTGecftnrhRz9oomf4qgY6FcSQsFAltWBI4ACgkQf4qgY6Fc
+SQuibAgAlhv/jqMGbJKaw5w8IkKxjGK1jgUILxbk90RW5Bc6NuKJiIE70xRS53Di
+ZGWflqF66NAwYv5jHxmnRH/mQZTtWxW8wXvixMuzFyqYSQrOEr4I8S5hzCj6i9cA
+ajOk/t4g3HsEDssRDb/+lDDCMe3ATG8Z7P7tk3OE+fxQGuOUtbiBLymnVLA1odQi
+rMBrD8/NcQxvQL62rfgiXuUnFoYYrrlppjngRhAF/l9F94jxXP4ZIXl0/fLPd/Th
+SBc92PmGhA5U6/rdnL/3sRGR+Upvk7LqP9I886fmFqH1OnHLmML3Qgn+UCsfbYmR
+7u2eHkzuxep6dNyCONWzaHQQdXg2wA==
+=Q6hl
 -----END PGP SIGNATURE-----
 */
