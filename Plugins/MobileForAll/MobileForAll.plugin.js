18a359bdad2398c3bdd51c947647afe42a721ca0
Mega-Mewthree <Mega-Mewthree@users.noreply.github.com>
2020-04-03 17:01:40 -0700

    Made switching from and to typing indicator less glitchy.

--- Plugins/MobileForAll/MobileForAll.plugin.js
+++ Plugins/MobileForAll/MobileForAll.plugin.js
@@ -24,7 +24,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
-// Updated January 20th, 2020.
+// Updated April 3rd, 2020.
 
 class MobileForAll {
   getName() {
@@ -37,12 +37,20 @@ class MobileForAll {
     return "Adds mobile indicators for all status types.\nRequired dependency: ZeresPluginLibrary\n\nMy Discord server: https://nebula.mooo.info/discord-invite\nDM me @Lucario ☉ ∝ x²#7902 or create an issue at https://github.com/Mega-Mewthree/BetterDiscordPlugins for support.";
   }
   getVersion() {
-    return "1.0.0";
+    return "1.0.1";
   }
   getAuthor() {
     return "Mega_Mewthree"; // Current Discord account: @Lucario ☉ ∝ x²#7902 (438469378418409483)
   }
-  constructor() {}
+  constructor() {
+    this.colors = {
+      online: "#43b581",
+      idle: "#faa61a",
+      dnd: "#f04747",
+      offline: "#747f8d",
+      streaming: "#593695"
+    };
+  }
   load() {}
   unload() {}
   start() {
@@ -59,28 +67,156 @@ class MobileForAll {
       BdApi.showToast(`${this.getName()} has started!`);
     }
   }
-  observer({addedNodes}) {
-    const targets = [].filter.call(addedNodes, n => /member/.test(n.className));
-    if (targets.length > 0) {
-      targets.forEach(target => {
-        try {
-          const userID = window.ZLibrary.ReactTools.getReactProperty(target, "return.return.return.return.return.return.memoizedProps.user.id");
-          if (!userID) return;
-          const userStatus = window.ZLibrary.DiscordModules.UserStatusStore.getState().clientStatuses[userID];
-          const platforms = Object.keys(userStatus);
-          if (platforms.length === 1 && platforms[0] === "mobile" && userStatus.mobile !== "online") {
-            const foreignObject = target.children[0].children[0].children[0].children[0].children[0];
-            const rect = target.children[0].children[0].children[0].children[0].children[1];
-            foreignObject.setAttribute("mask", "url(#svg-mask-avatar-status-mobile-32)");
-            rect.setAttribute("mask", "url(#svg-mask-status-online-mobile)");
-            rect.setAttribute("height", 15);
-            rect.setAttribute("y", 17);
-          }
-        } catch (e) {}
-      });
+  observer({addedNodes, removedNodes, target}) {
+    // Added Nodes Section
+    if (addedNodes.length > 0) {
+      // Switching Guilds
+      const guildSwitchMemberListTargets = Array.from(
+        [].filter.call(addedNodes, n => /members-/.test(n.parentElement && n.parentElement.getAttribute("data-ref-id")))
+        .flatMap(n => [].slice.call(n.children[1].children))
+      );
+      const guildMemberListChangeTargets = [].filter.call(addedNodes, n => /member-/.test(n.className));
+      const guildMemberListTargets = [...guildSwitchMemberListTargets, ...guildMemberListChangeTargets];
+      if (guildMemberListTargets.length > 0) {
+        guildMemberListTargets.forEach(t => {
+          try {
+            const userID = window.ZLibrary.ReactTools.getReactProperty(t, "return.return.return.return.return.return.memoizedProps.user.id");
+            if (!userID) return;
+            const userStatus = this.getUserStatus(userID);
+            if (!userStatus) return;
+            const platforms = Object.keys(userStatus);
+            if (platforms.length === 1 && platforms[0] === "mobile" && userStatus.mobile !== "online") {
+              let offset = 0;
+              const parent = t.children[0].children[0].children[0].children[0];
+              if (parent.children[0].tagName.toLowerCase() === "mask") offset = 1;
+              const foreignObject = parent.children[offset];
+              const rect = parent.children[offset * 2 + 1]; // After typing, foreignObject is second and rect is fourth.
+
+              if (this.getUserTypingStatus(userID)) {
+                [].find.call(parent.children, c => c.tagName.toLowerCase() === "svg").style.display = null;
+                foreignObject.setAttribute("mask", `url(#svg-mask-avatar-status-typing-32)`);
+                if (offset) {
+                  rect.mask = null;
+                  rect.setAttribute("height", 10);
+                  rect.setAttribute("y", 22);
+                }
+                return;
+              }
+
+              foreignObject.setAttribute("mask", "url(#svg-mask-avatar-status-mobile-32)");
+              rect.setAttribute("mask", "url(#svg-mask-status-online-mobile)");
+              rect.setAttribute("height", 15);
+              rect.setAttribute("y", 17);
+              if (offset) {
+                [].find.call(parent.children, c => c.tagName.toLowerCase() === "svg").style.display = "none";
+              }
+            }
+          } catch (e) {}
+        });
+      }
+      const guildMemberListUpdateTargets = [].filter.call(addedNodes, n => n.tagName && n.tagName.toUpperCase() === "SVG");
+      if (guildMemberListUpdateTargets.length > 0) {
+        guildMemberListUpdateTargets.forEach(t => {
+          try {
+            t = t.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement;
+            if (!/member-/.test(t.className)) return;
+            const userID = window.ZLibrary.ReactTools.getReactProperty(t, "return.return.return.return.return.return.memoizedProps.user.id");
+            if (!userID) return;
+            const userStatus = this.getUserStatus(userID);
+            if (!userStatus) return;
+            const platforms = Object.keys(userStatus);
+            if (platforms.length === 1 && platforms[0] === "mobile" && userStatus.mobile !== "online") {
+              let offset = 0;
+              const parent = t.children[0].children[0].children[0].children[0];
+              if (parent.children[0].tagName.toLowerCase() === "mask") offset = 1;
+              const foreignObject = parent.children[offset];
+              const rect = parent.children[offset * 2 + 1]; // After typing, foreignObject is second and rect is fourth.
+
+              if (this.getUserTypingStatus(userID)) {
+                [].find.call(parent.children, c => c.tagName.toLowerCase() === "svg").style.display = null;
+                foreignObject.setAttribute("mask", `url(#svg-mask-avatar-status-typing-32)`);
+                if (offset) {
+                  rect.removeAttribute("mask");
+                  rect.setAttribute("fill", "transparent");
+                  rect.setAttribute("height", 10);
+                  rect.setAttribute("y", 22);
+                }
+                return;
+              }
+
+              foreignObject.setAttribute("mask", "url(#svg-mask-avatar-status-mobile-32)");
+              rect.setAttribute("mask", "url(#svg-mask-status-online-mobile)");
+              rect.setAttribute("height", 15);
+              rect.setAttribute("y", 17);
+              if (offset) {
+                rect.setAttribute("fill", this.colors[userStatus.mobile]);
+                [].find.call(parent.children, c => c.tagName.toLowerCase() === "svg").style.display = "none";
+              }
+            }
+          } catch (e) {}
+        });
+      }
+    }
+
+    // Removed Nodes Section
+    if (removedNodes.length > 0) {
+      const guildMemberListUpdateTargets = [].filter.call(removedNodes, n => n.tagName && n.tagName.toLowerCase() === "svg");
+      if (guildMemberListUpdateTargets.length > 0) {
+        guildMemberListUpdateTargets.forEach(t => {
+          try {
+            t = target.parentElement.parentElement.parentElement.parentElement.parentElement;
+            if (!/member-/.test(t.className)) return;
+            const userID = window.ZLibrary.ReactTools.getReactProperty(t, "return.return.return.return.return.return.memoizedProps.user.id");
+            if (!userID) return;
+            const userStatus = this.getUserStatus(userID);
+            const platforms = Object.keys(userStatus);
+            if (platforms.length === 1 && platforms[0] === "mobile" && userStatus.mobile !== "online") {
+              let offset = 0;
+              const parent = t.children[0].children[0].children[0].children[0];
+              if (parent.children[0].tagName.toLowerCase() === "mask") offset = 1;
+              const foreignObject = parent.children[offset];
+              const rect = parent.children[offset * 2 + 1]; // After typing, foreignObject is second and rect is fourth.
+
+              if (this.getUserTypingStatus(userID)) {
+                [].find.call(parent.children, c => c.tagName.toLowerCase() === "svg").style.display = null;
+                foreignObject.setAttribute("mask", `url(#svg-mask-avatar-status-typing-32)`);
+                if (offset) {
+                  rect.removeAttribute("mask");
+                  rect.setAttribute("fill", "transparent");
+                  rect.setAttribute("height", 10);
+                  rect.setAttribute("y", 22);
+                }
+                return;
+              }
+
+              foreignObject.setAttribute("mask", "url(#svg-mask-avatar-status-mobile-32)");
+              rect.setAttribute("mask", "url(#svg-mask-status-online-mobile)");
+              rect.setAttribute("height", 15);
+              rect.setAttribute("y", 17);
+              if (offset) {
+                rect.setAttribute("fill", this.colors[userStatus.mobile]);
+                [].find.call(parent.children, c => c.tagName.toLowerCase() === "svg").style.display = "none";
+              }
+            }
+          } catch (e) {}
+        });
+      }
     }
   }
-  async stop() {
+  stop() {
     BdApi.showToast(`${this.getName()} has stopped!`);
+  }
+  getUserStatus(userID) {
+    return window.ZLibrary.DiscordModules.UserStatusStore.getState().clientStatuses[userID];
+  }
+  getUserTypingStatus(userID) {
+    const channelID = window.ZLibrary.DiscordModules.SelectedChannelStore.getChannelId();
+    if (!channelID) return null;
+    const channelTypingUsers = window.ZLibrary.DiscordModules.UserTypingStore.getTypingUsers(channelID);
+    if (channelTypingUsers[userID]) return true;
+    return false;
+  }
+  getMaskIDs() {
+    return window.ZLibrary.WebpackModules.getByProps("MaskIDs").MaskIDs;
   }
 }
